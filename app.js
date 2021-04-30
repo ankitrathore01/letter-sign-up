bf95eb15987cb157e6132e3f6c174e259ed202b6
Ankit Rathore <rathoreankit01091999@gmail.com>
2021-04-30 17:48:03 +0530

    Added Procfile and update the files

--- app.js
+++ app.js
@@ -55,7 +55,12 @@ app.post("/failure.html", function(req, res) {
   res.redirect("/");
 });
  
-app.listen(process.env.PORT|| 3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server is running on port 3000.");
 });
 
